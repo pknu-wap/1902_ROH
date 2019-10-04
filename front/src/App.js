0b3e617f91f934d6decead26f61edf4f20a0a55a
JeongbinPark <qkrwjdqls99@gmail.com>
2019-10-04 15:32:05 +0900

    Add Router, Route, Link

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Wap from './Wap';
+import Detail from './Detail';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-       <Wap />
+      <Router>
+        <Route path="/detail" component={Detail}/>
+        <Route exact path="/" component={Wap}/>
+      </Router>
       </div>
     );
-  } 
+  }
 }
 
 export default App;
--- front/src/Wap.js
+++ front/src/Wap.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Wap.css';
 
 class Wap extends Component{
@@ -16,12 +17,12 @@ class Wap extends Component{
 class Header extends Component{
     render(){
      return(
-     
+
         <div className = "container">
         <h1>부경대 화장실 찾기</h1>
         <a>-대연캠퍼스-</a>
         </div>
-     
+
         );
     }
 }
@@ -43,11 +44,11 @@ class Marker extends Component{
         document.getElementById("popup1").style.zIndex="-1";
     }
 
-    render(){       
-        
+    render(){
+
         return(
             <div class = "mark">
-          
+
           <a id = "marker1" onClick={this.myFunction} >
           <img src = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"/>
           </a>
@@ -64,20 +65,20 @@ class Marker extends Component{
          );
     }
 }
- 
+
 
 
 class Infort extends Component{
     render(){
         return(
-        
+
             <div id = "popup1" >
-            <p>남자 화장실 갯수 : 총 67개</p>
+            <Link to="/detail"><p>남자 화장실 갯수 : 총 67개</p></Link>  {/*화장실 별로 하나씩 모두 Link*/}
             <p>여자 화장실 갯수 : 총 개</p>
             <a href = "#">자세히 보기</a>
             </div>
-       
-        
+
+
         );
     }
 }
