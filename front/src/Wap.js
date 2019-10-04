ac01d70e380d2abb28fc7ef3e3f2a9744e2efebc
JeongbinPark <qkrwjdqls99@gmail.com>
2019-10-04 16:26:54 +0900

    Add toggle

--- front/src/Wap.js
+++ front/src/Wap.js
@@ -6,23 +6,20 @@ class Wap extends Component{
     render(){
         return(
             <div>
-      <Header />
-      <Content />
-      </div>
+              <Header />
+              <Content />
+            </div>
         );
     }
 }
 
-
 class Header extends Component{
     render(){
-     return(
-
+      return(
         <div className = "container">
-        <h1>부경대 화장실 찾기</h1>
-        <a>-대연캠퍼스-</a>
+          <h1>부경대 화장실 찾기</h1>
+          <h3>-대연캠퍼스-</h3>
         </div>
-
         );
     }
 }
@@ -31,57 +28,65 @@ class Content extends Component{
     render(){
         return(
            <div id= "img">
-           <Marker />
-           <Infort />
+            <Toggle id="1"/>
+            {/*Toggle 컴포넌트로 id 값을 주어서 마커와 Infort 하나씩 생성*/}
            </div>
-                );
-    }
-}
-
-
-class Marker extends Component{
-     myFunction(){
-        document.getElementById("popup1").style.zIndex="-1";
-    }
-
-    render(){
-
-        return(
-            <div class = "mark">
-
-          <a id = "marker1" onClick={this.myFunction} >
-          <img src = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"/>
-          </a>
-          <a href = "" id = "marker2" onClick = "javascript:myFunction();" >
-              <img src = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"/>
-          </a>
-          <a href = "" id = "marker3" onClick = "javascript:myFunction();" >
-              <img src = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"/>
-          </a>
-          <a href = "" id = "marker4" onClick = "javascript:myFunction();" >
-              <img src = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"/>
-          </a>
-        </div>
          );
     }
 }
 
-
+class Marker extends Component{
+    render(){
+      return(
+        <div className = "mark">
+          <p id = {this.props.id}>
+          <img alt="marker" src = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"/>
+          </p>
+        </div>
+      );
+    }
+}
 
 class Infort extends Component{
     render(){
         return(
-
-            <div id = "popup1" >
-            <Link to="/detail"><p>남자 화장실 갯수 : 총 67개</p></Link>  {/*화장실 별로 하나씩 모두 Link*/}
-            <p>여자 화장실 갯수 : 총 개</p>
-            <a href = "#">자세히 보기</a>
+            <div id = {this.props.id} >
+            <Link to="/detail"><p>1층 중앙 남자 화장실</p></Link>
+            <Link to="/detail"><p>1층 중앙 남자 화장실</p></Link>
+            <Link to="/detail"><p>1층 중앙 남자 화장실</p></Link>
+            <Link to="/detail"><p>1층 중앙 남자 화장실</p></Link>
+            {/*
+              나중에
+              <Link to="/detail/화장실_id값"><p>{층}층 {위치} {남/여} 화장실</p></Link>
+            */}
             </div>
-
-
         );
     }
 }
 
+class Toggle extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      hidden : false,
+      marker_id : "marker" + this.props.id,
+      popup_id : "popup" + this.props.id
+    }
+  }
+  _clickHandler=()=>{
+    this.setState({
+      hidden: !this.state.hidden
+    });
+  }
+  render(){
+    console.log(this.state.marker_id, this.state.popup_id);
+    return (
+      <div onClick={this._clickHandler}>
+        <Marker id={this.state.marker_id}/>
+        { this.state.hidden ? null : <Infort id={this.state.popup_id}/>  }
+      </div>
+    )
+  }
+}
 
 export default Wap;
